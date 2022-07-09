27dd5b7c0bdc401fe53dbc8ec8886791cc6c5235
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-07-09 12:55:17 +0100

    Show icon on load

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,2 +1,17 @@
 import "@fortawesome/fontawesome-free/js/all";
 import "../styles/main.scss";
+
+const load = function onPageLoadShowIcon() {
+  const menus = document.querySelectorAll("#menu");
+
+  menus?.forEach((menu) => {
+    const hamburger = document.createElement("div");
+    hamburger.classList.add("hamburger");
+    const i = document.createElement("i");
+    i.classList.add("fas", "fa-bars");
+    hamburger.appendChild(i);
+    menu?.appendChild(hamburger);
+  });
+};
+
+window.addEventListener("load", load);
